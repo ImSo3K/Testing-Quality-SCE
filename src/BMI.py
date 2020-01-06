db0f15e6edbf5c0202e683e99d733c3b29722d8f
Sharon Yaroshetsky <imso3k@gmail.com>
2020-01-06 19:23:08 +0200

    renamed folders

---
+++ src/BMI.py
@@ -0,0 +1,15 @@
+def calculate_BMI(maesurments: tuple):
+    try:
+        BMI = maesurments[1] / (maesurments[0] ** 2)
+    except ZeroDivisionError as err:
+        return ZeroDivisionError
+    return BMI
+
+
+def BMI_category(bmi):
+    if bmi < 18.5:
+        return 'underweight'
+    elif bmi > 25:
+        return 'obese'
+    else:
+        return 'normal-weight'
---
+++ test/BMI_test.py
@@ -0,0 +1,46 @@
+import unittest
+from src_BMI import BMI
+
+
+class BMI_test(unittest.TestCase):
+    def test_BMI_calculation(self):
+        # stub
+        stub1 = (1.72, 87.3)
+        stub2 = (0, 87.3)
+
+        # expected
+        expected1 = float
+        expected2 = ZeroDivisionError
+
+        # action
+        result1 = BMI.calculate_BMI(stub1)
+        result2 = BMI.calculate_BMI(stub2)
+
+        # actual check
+        self.assertIsInstance(result1, expected1)
+        self.assertEqual(result2, expected2)
+
+    def test_BMI_category(self):
+        # stub
+        stub1 = 17.3
+        stub2 = 22.2
+        stub3 = 27.8
+
+        # excpected
+        exptected1 = 'underweight'
+        exptected2 = 'normal-weight'
+        exptected3 = 'obese'
+
+        # action
+        result1 = BMI.BMI_category(stub1)
+        result2 = BMI.BMI_category(stub2)
+        result3 = BMI.BMI_category(stub3)
+
+        # actual check
+        self.assertEqual(exptected1, result1)
+        self.assertEqual(exptected2, result2)
+        self.assertEqual(exptected3, result3)
+
+
+if __name__ == '__main__':
+    unittest.main()
