ef5d577306ae556a6999605eba8be0f0232532fa
“your <IlanKroter@gmail.com>
2020-01-08 21:45:27 +0200

    0.1.3 refactored the bubblesort to do the swap in one line

--- src/BubbleSort.py
+++ src/BubbleSort.py
@@ -7,7 +7,7 @@ def BubbleSort(alist):
     for passnum in range(len(alist)-1,0,-1):
         for i in range(passnum):
             if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i]=alist[i+1]
-                alist[i + 1]=temp
+                alist[i],alist[i+1]=alist[i+1],alist[i]
     return alist
+
+
