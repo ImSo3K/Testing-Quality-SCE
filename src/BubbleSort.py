ee924dbe960e2b64bb353dc79f5b9d83bc71685e
“your <IlanKroter@gmail.com>
2020-01-08 20:33:26 +0200

    0.1.1. fixed the tests so they actually work, and fixed no return in the function

--- src/BubbleSort.py
+++ src/BubbleSort.py
@@ -1,7 +1,13 @@
-def bubbleSort(alist):
+def BubbleSort(alist):
+    """
+    Sorts a collection using bubble sort.
+    :param alist: A list
+    :return: A sorted list using bubble sort
+    """
     for passnum in range(len(alist)-1,0,-1):
         for i in range(passnum):
             if alist[i]>alist[i+1]:
                 temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
+                alist[i]=alist[i+1]
+                alist[i + 1]=temp
+    return alist
--- test/UnitTestBubble.py
+++ test/UnitTestBubble.py
@@ -1,4 +1,6 @@
 import unittest
+from functools import reduce
+from src.BubbleSort import BubbleSort
 
 
 class MyTestCase(unittest.TestCase):
@@ -8,54 +10,49 @@ class MyTestCase(unittest.TestCase):
         Tests if the collection contains the same members after the bubble sort
         """
         #stub
-        stub1 = (1,2,3,4,2,1,2,34,45,32,12,4)
-        stub2 = (13, 22, 31, 412, 24, 13, 22, 12, 45, 32, 12, 41)
-        stub3 = (123, 212, 313, 412, 24, 153, 262, 172, 415, 32, 12, 41)
+        stub1 = [1,2,3,4,2,1,2,34,45,32,12,4]
+        stub2 = [13, 22, 31, 412, 24, 13, 22, 12, 45, 32, 12, 41]
+        stub3 = [123, 212, 313, 412, 24, 153, 262, 172, 415, 32, 12, 41]
 
-        #expected result
-        expected1= {stub1} #put the stub in set
-        expected2= {stub2}
-        expected3 = {stub3}
+        #check integrity func
+        def Check_Integrity(alist,blist):
+            return all(x in alist for x in blist)
         #test
-        self.assertSetEqual({BubbleSort.sort(stub1)},expected1)
-        self.assertSetEqual({BubbleSort.sort(stub2)},expected2)
-        self.assertSetEqual({BubbleSort.sort(stub3)},expected3)
+        self.assertTrue(Check_Integrity(BubbleSort(stub1),stub1))
+        self.assertTrue((Check_Integrity(BubbleSort(stub2),stub2)))
+        self.assertTrue(Check_Integrity(BubbleSort(stub3),stub3))
 
     def test_BubbleSort_sorted(self):
         """
         tests if the bubblesort sorts the collection
         """
         #stub
-        stub1 = (1,2,-3,4,2,1,2,34,45,32,12,4)
+        stub1 = [1,2,-3,4,2,1,2,34,45,32,12,4]
         stub2 = [13, -22, 31, 412, 24, 13, 22, 12, 45, 32, 12, 41]
         stub3 = [-122, 212, 313, 412, 24, 153, 262, 172, 415, 32, 12, 41]
 
-        #expected result
-        expected1= sorted(stub1)#put the stub in set
-        expected2= sorted(stub2)
-        expected3 = sorted(stub3)
+        #tester function
+        def TestSorted(alist):
+            return reduce(lambda x,y:x>y,alist)is False # if all the values are false it's sorted if one is true the array is not sorted.
+
         #test
-        self.assertSetEqual(BubbleSort.sort(stub1),expected1)
-        self.assertSetEqual(BubbleSort.sort(stub2),expected2)
-        self.assertSetEqual(BubbleSort.sort(stub3),expected3)
+        self.assertTrue(TestSorted(BubbleSort(stub1)))
+        self.assertTrue(TestSorted(BubbleSort(stub2)))
+        self.assertTrue(TestSorted(BubbleSort(stub3)))
 
     def test_BubbleSort_value(self):
         """
         tests if the bubblesort returns any value
         """
         #stub
-        stub1 = (1,2,-3,4,2,1,2,34,45,32,12,4)
+        stub1 = [1,2,-3,4,2,1,2,34,45,32,12,4]
         stub2 = [13, -22, 31, 412, 24, 13, 22, 12, 45, 32, 12, 41]
         stub3 = [-122, 212, 313, 412, 24, 153, 262, 172, 415, 32, 12, 41]
 
-        #expected result
-        expected1= sorted(stub1)#put the stub in set
-        expected2= sorted(stub2)
-        expected3 = sorted(stub3)
         #test
-        self.assertTrue(BubbleSort.sort(stub1),expected1)
-        self.assertTrue(BubbleSort.sort(stub2),expected2)
-        self.assertTrue(BubbleSort.sort(stub3),expected3)
+        self.assertTrue(BubbleSort(stub1))
+        self.assertTrue(BubbleSort(stub2))
+        self.assertTrue(BubbleSort(stub3))
 
 
 
