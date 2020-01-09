e0a301e931321ae09f13d84d1723f3289f34bb84
noynirr <noynir100@gmail.com>
2020-01-09 17:54:32 +0200

    0.1.9 refactored BMI category and added maching tests

--- src/BMI.py
+++ src/BMI.py
@@ -2,14 +2,14 @@ def calculate_BMI(maesurments: tuple):
     try:
         BMI = maesurments[1] / (maesurments[0] ** 2)
     except ZeroDivisionError as err:
-        return err
+        raise err
     return BMI
 
 
 def BMI_category(bmi):
-    if bmi < 18.5:
-        return 'underweight'
-    elif bmi > 25:
-        return 'obese'
-    else:
-        return 'normal-weight'
+    bmiDict={18.5:'underweight',25:'normal-weight',29.9:'excess-weight'
+        ,34.9:'obese-L1',39.9:'obese-L2',40:'obese-L3'}
+    for item in bmiDict:
+        if bmi < item:
+            return bmiDict[item]
+    return bmiDict[40]
